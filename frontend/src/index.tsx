a311d468e5af42803fda93fdb24a805d02f824e1
Islam Markhiev <markhiev612@mail.ru>
2024-02-10 23:10:53 +0300

    webpack updates

---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import  ReactDOM  from "react-dom/client";
+import Counter from "./components/Counter";
+import './index.scss'
+ReactDOM.createRoot(document.getElementById("root")).render(
+<div className="app">
+<Counter/>
+</div>
+
+)
