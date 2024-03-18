0be32f5dbde6ba0df7f7a13c50283bdbaeca04d9
katsiev1997 <katsiev1997@gmail.com>
2024-03-18 22:57:14 +0300

    table

---
+++ frontend/src/widgets/Table/ui/Table.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+
+export const Table = () => {
+  return (
+    <div className="m-10">
+      <table className="shadow-2xl w-full">
+        <thead className="text-white bg-tableHead text-3xl">
+          <th className="px-6 py-3 text-center font-bold">#</th>
+          <th className="px-6 py-3 text-center font-bold">КОМАНДА</th>
+          <th className="px-6 py-3 text-center font-bold">И</th>
+          <th className="px-6 py-3 text-center font-bold">В</th>
+          <th className="px-6 py-3 text-center font-bold">Н</th>
+          <th className="px-6 py-3 text-center font-bold">П</th>
+          <th className="px-6 py-3 text-center font-bold">РМ</th>
+          <th className="px-6 py-3 text-center font-bold">О</th>
+        </thead>
+        <tbody>
+          <tr className="hover:bg-gray-100 text-xl">
+            <td className="text-3xl text-center py-3 px-6 font-semibold">1</td>
+            <td className="text-center py-3 px-6 font-semibold">
+              Легион (Махачкала)
+            </td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0:0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+          </tr>
+          <tr className="hover:bg-gray-100 text-xl">
+            <td className="text-3xl text-center py-3 px-6 font-semibold">1</td>
+            <td className="text-center py-3 px-6 font-semibold">
+              Легион (Махачкала)
+            </td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0:0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+          </tr>
+          <tr className="hover:bg-gray-100 text-xl">
+            <td className="text-3xl text-center py-3 px-6 font-semibold">1</td>
+            <td className="text-center py-3 px-6 font-semibold">
+              Легион (Махачкала)
+            </td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0:0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+          </tr>
+          <tr className="hover:bg-gray-100 text-xl">
+            <td className="text-3xl text-center py-3 px-6 font-semibold">1</td>
+            <td className="text-center py-3 px-6 font-semibold">
+              Легион (Махачкала)
+            </td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0:0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+          </tr>
+          <tr className="hover:bg-gray-100 text-xl">
+            <td className="text-3xl text-center py-3 px-6 font-semibold">1</td>
+            <td className="text-center py-3 px-6 font-semibold">
+              Легион (Махачкала)
+            </td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0:0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+          </tr>
+          <tr className="hover:bg-gray-100 text-xl">
+            <td className="text-3xl text-center py-3 px-6 font-semibold">1</td>
+            <td className="text-center py-3 px-6 font-semibold">
+              Легион (Махачкала)
+            </td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+            <td className="text-center py-3 px-6 font-semibold">0:0</td>
+            <td className="text-center py-3 px-6 font-semibold">0</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tableHead: "#1B4332E5",
+      },
+    },
   },
   plugins: [],
 };
