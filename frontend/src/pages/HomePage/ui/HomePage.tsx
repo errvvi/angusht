c026d9df1d64a79049a53a40d230c77aa00e73f3
akkerman06 <islamhaluhaev225@gmail.com>
2024-03-18 22:30:12 +0300

    fix homePage News

--- frontend/src/pages/HomePage/ui/HomePage.tsx
+++ frontend/src/pages/HomePage/ui/HomePage.tsx
@@ -2,13 +2,11 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { ArticleCard } from "@/widgets/ArticleCard/ui/ArticleCard";
 import { Icon } from "@/shared/ui/Icon/Icon";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   const [articles, setArticles] = useState([]);
-  const [visibleArticles, setVisibleArticles] = useState(true);
-  const toggleArticles = () => {
-    setVisibleArticles(!visibleArticles);
-  };
+
   useEffect(() => {
     const getPosts = async () => {
       const { data } = await axios.get(
@@ -23,26 +21,26 @@ function HomePage() {
   return (
     <div className="bg-customGreen">
       <div className="Articles-part">
-        <div
-          onClick={toggleArticles}
-          className="ml-12 flex items-center gap-3 cursor-pointer"
-        >
-          <Icon type="Vector" />
-          <span className="font-black text-white text-2xl">НОВОСТИ КЛУБА</span>
-        </div>
+        <Link to={"/news"}>
+          <div className="ml-12 flex items-center gap-3 cursor-pointer">
+            <Icon type="Vector" />
+            <span className="font-black text-white text-2xl">
+              НОВОСТИ КЛУБА
+            </span>
+          </div>
+        </Link>
+
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 ml-12">
-          {visibleArticles
-            ? articles
-                .slice(-6)
-                .reverse()
-                .map((article) => (
-                  <ArticleCard
-                    key={article.id}
-                    id={article.id}
-                    title={article.title}
-                  />
-                ))
-            : null}
+          {articles
+            .slice(-6)
+            .reverse()
+            .map((article) => (
+              <ArticleCard
+                key={article.id}
+                id={article.id}
+                title={article.title}
+              />
+            ))}
         </div>
       </div>
     </div>
