6b37e002c6bd3646844921bc139d3f38741cdbd3
akkerman06 <islamhaluhaev225@gmail.com>
2024-03-19 18:00:20 +0300

    update news

--- frontend/src/pages/NewsPage/ui/NewsPage.tsx
+++ frontend/src/pages/NewsPage/ui/NewsPage.tsx
@@ -1,17 +1,26 @@
 import { ArticleCard } from "@/widgets/ArticleCard/ui/ArticleCard";
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function NewsPage() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const { data } = await axios.get(
+        "https://65f85cccdf151452460f3434.mockapi.io/api/articles"
+      );
+      setArticles(data);
+    };
+
+    getPosts();
+  }, []); // ИМИТАЦИЯ
   return (
     <div className="bg-customGreen">
       <div className=" grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3  ml-12">
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
+        {articles.reverse().map((article) => (
+          <ArticleCard key={article.id} id={article.id} title={article.title} />
+        ))}
       </div>
     </div>
   );
