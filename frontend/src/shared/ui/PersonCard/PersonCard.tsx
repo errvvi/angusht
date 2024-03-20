cbc22dbc80a43c6933139e71a74f067e01076405
katsiev1997 <katsiev1997@gmail.com>
2024-03-21 02:31:21 +0300

    person card v1

---
+++ frontend/src/shared/ui/PersonCard/PersonCard.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+
+
+interface PersonProps {
+  img: string;
+  jobTitle: string;
+  fio: string;
+  id: string;
+}
+
+export const PersonCard: FC<PersonProps> = ({ img, jobTitle, fio, id }) => {
+  return (
+    <div className="flex flex-col w-72">
+      <img className="object-cover" src={img} alt="photo" />
+      <div className="h-32 flex flex-col justify-start items-center gap-4 bg-white rounded-b-lg">
+        <p className="text-xl">{jobTitle}</p>
+        <h3 className="text-3xl text-center font-semibold">{fio}</h3>
+      </div>
+    </div>
+  );
+};
